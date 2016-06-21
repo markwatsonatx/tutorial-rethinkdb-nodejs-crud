7fe8013cb0796679412965d96cc35e17027321fd
Mark Watson <mark.watson.x@gmail.com>
2016-06-20 21:40:42 -0500

    Initial commit

---
+++ src/app/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rethinkdb: {
+        host: "192.168.99.100",
+        port: 28015,
+        authKey: "",
+        db: "game"
+    },
+    express: {
+        port: 3000
+    }
+}
---
+++ src/app/server.js
@@ -0,0 +1,63 @@
+var config = require('./config.js');
+var express = require('express');
+var gameController = require('./controllers/gameController.js');
+var r = require('rethinkdb');
+
+var app = express();
+
+(function(app) {
+    r.connect(config.rethinkdb, function(err, conn) {
+        if (err) {
+            console.log('Could not open a connection to initialize the database: ' + err.message);
+        }
+        else {
+            console.log('Connected.');
+            app.set('rdbConn', conn);
+            gameController.createDatabase(conn, config.rethinkdb.db)
+                .then(function() {
+                    return gameController.createTable(conn, 'games');
+                })
+                .catch(function(err) {
+                    console.log('Error creating database and/or table: ' + err);
+                })
+        }
+    });
+})(app);
+
+// set view engine and map views directory
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// map requests
+app.get('/', function(req, res) {
+    gameController.getGames(req)
+        .then(function(games) {
+            res.render('index', {games: games});
+        });
+});
+
+app.post('/create', function(req, res) {
+    gameController.createGame(req)
+        .then(function() {
+            res.redirect("/");
+        });
+});
+
+app.post('/update', function(req, res) {
+    gameController.updateGame(req)
+        .then(function() {
+            res.redirect("/");
+        });
+});
+
+app.post('/delete', function(req, res) {
+    gameController.deleteGame(req)
+        .then(function() {
+            res.redirect("/");
+        });
+});
+
+// start server on the specified port and binding host
+app.listen(config.express.port, '0.0.0.0', function() {
+  console.log("Server started.")
+});
