0d0d47fc7cd41bfacb7b926f7fb221455ea28587
Mark Watson <mark.watson.x@gmail.com>
2017-07-04 20:59:53 -0500

    npm cache testing

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /usr/src/app
+npm config set ca=""
 if [[ ! -z ${NPM_PROXY_CACHE} ]]; then
-	echo "NPM_PROXY_CACH=${NPM_PROXY_CACHE}"
-    npm --proxy ${NPM_PROXY_CACHE} --https-proxy ${NPM_PROXY_CACHE} --strict-ssl false install
+	npm --proxy ${NPM_PROXY_CACHE} --https-proxy ${NPM_PROXY_CACHE} --strict-ssl false install
 	npm --proxy ${NPM_PROXY_CACHE} --https-proxy ${NPM_PROXY_CACHE} --strict-ssl false install -g nodemon
 else
 	npm install
