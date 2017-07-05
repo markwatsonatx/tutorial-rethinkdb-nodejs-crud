e22a5ac4e0d3a85b21a29cbd8195c9e714d73969
Mark Watson <mark.watson.x@gmail.com>
2017-07-04 22:17:54 -0500

    Temporary workaround to bug in npm 5.0.3

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /usr/src/app
 if [[ ! -z ${NPM_PROXY_CACHE} ]]; then
-	echo "npm --proxy ${NPM_PROXY_CACHE} --https-proxy ${NPM_PROXY_CACHE} --strict-ssl false install"
+	# This is temporary until npm 5.0.4 is released (see https://github.com/npm/npm/issues/16868)
+	export NODE_TLS_REJECT_UNAUTHORIZED="0"
 	npm --proxy ${NPM_PROXY_CACHE} --https-proxy ${NPM_PROXY_CACHE} --strict-ssl false install
 	npm --proxy ${NPM_PROXY_CACHE} --https-proxy ${NPM_PROXY_CACHE} --strict-ssl false install -g nodemon
 else
